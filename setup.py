62e8ab1812a44d5ee27cdc584a072a938d9b4ce2
AndoniainaNantenaina <ando.andoniaina.nantenaina@esti.mg>
2024-09-11 10:16:35 +0300

    Adding setup.py contents and managing project structure.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fileman',
+    version='0.1',
+    description='A simple Python library for managing files and directories',
+    author='Andoniaina Nomenjanahary',
+    packages=find_packages(),
+)
